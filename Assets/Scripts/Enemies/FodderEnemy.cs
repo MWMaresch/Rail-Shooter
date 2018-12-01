25c6bb5b62058b4002a6136d9d7d984dea88b981
HappyHead <netogmoshe@gmail.com>
2018-12-01 22:03:57 +0200

    finished player spritesheet
    -finished player spritesheet
    -made fodder enemy move only forawrd (sorry for the messy code)
    -tweaked enemy timings

--- Assets/Scripts/Enemies/FodderEnemy.cs
+++ Assets/Scripts/Enemies/FodderEnemy.cs
@@ -35,10 +35,10 @@ public class FodderEnemy : Enemy {
 
             //move towards the player, but not if we're too close already
             float distance = Vector3.Distance(transform.position, player.transform.position);
-            if (distance > rammingDistance)
+            if (distance > 0)
             {
-                direction = (player.transform.position - transform.position).normalized;
-                transform.position += direction * speed;
+                //direction = (player.transform.position - transform.position).normalized;
+                transform.position += Vector3.back * speed;
 
                 //every set amount of time, shoot at the player
                 shootTimer -= Time.fixedDeltaTime;
